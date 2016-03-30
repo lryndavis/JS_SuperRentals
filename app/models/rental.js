b04fb5d10d6b4658f52df54f65df898169614ca1
Lauryn Davis <lryndavis@gmail.com>
2016-03-29 17:47:00 -0700

    create ability to add new cities to rental app

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   owner: DS.attr(),
-  city: DS.attr(),
+  city: DS.belongsTo('city', { async: true }),
   type: DS.attr(),
   image: DS.attr(),
   bedrooms: DS.attr()
-  
+
 });
